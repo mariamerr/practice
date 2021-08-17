0cf257cc25ef4e47e82d7b7e0e3b707244a1f26c
mariamer <mermarand@mail.ru>
2021-08-17 20:38:53 +0300

    second

---
+++ src/learning/VehicleDemo.java
@@ -0,0 +1,13 @@
+class Vehicle {
+    int a;
+    int b;
+}
+
+class VehicleDemo {
+    public static void main(String[] args) {
+        Vehicle car1 = new Vehicle();
+        car1.a = 2;
+        car1.b = 4;
+        System.out.println("lloooll");
+    }
+}
